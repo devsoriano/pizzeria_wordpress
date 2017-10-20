b25e8ff96c85f367ddd40571223ac55dc684926c
Rafael Soriano <rafa.soriano.ramirez@gmail.com>
2017-10-20 17:54:17 -0500

    agregar estructura de template, ejemplo simple de llamado de plantilla

--- wp-content/themes/lapizzeria/index.php
+++ wp-content/themes/lapizzeria/index.php
@@ -1,6 +1,11 @@
-<?php 
-while (have_posts()):the_post();
-    the_title('<h1>', '</h1>');
-
-    the_content();
-endwhile;
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <h1>Hola!!!</h1>
+</body>
+</html>
