3b81739500ccce147a254cfcc6e87b31114eb3cf
Rafael Soriano <rafa.soriano.ramirez@gmail.com>
2017-10-23 11:28:59 -0500

    Estructura básica de mapa de gerarquización de páginas en WP

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -1,6 +1,12 @@
 <?php
 function lapizzeria_styles() {
-    wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0' );
+    //Registrar estilos
+    wp_register_style( 'style', get_template_directory_uri() . '/css/normalize.css', array(), '7.0.0' );
+    wp_register_style( 'style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0' );
+
+    //Llamar estilos
+    wp_enqueue_style( 'normalize' );
+    wp_enqueue_style( 'style' );
 }
 
 add_action( 'wp_enqueue_scripts', 'lapizzeria_styles' );
