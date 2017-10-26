7da0354e839295712b1d8870ccb123491bebf6cc
Rafael Soriano <rafa.soriano.ramirez@gmail.com>
2017-10-26 18:13:33 -0500

    plantilla nosotros completa

--- wp-content/themes/lapizzeria/js/scripts.js
+++ wp-content/themes/lapizzeria/js/scripts.js
@@ -9,10 +9,26 @@ $ ( document ).ready( function () {
   //resize a pantalla para no desaparecer menú responsivo
   var breackpoint = 768;
   $( window ).resize( function () {
-      if ( $( document ).width() >= breackpoint ) {
-        $( 'nav.menu-sitio' ).show();
-      } else {
-        $( 'nav.menu-sitio' ).hide();
-      }
+    ajustarCajas();
+    if ( $( document ).width() >= breackpoint ) {
+      $( 'nav.menu-sitio' ).show();
+    } else {
+      $( 'nav.menu-sitio' ).hide();
+    }
   });
+
+  //llamar a ajustarCajas  
+  ajustarCajas();
 });
+
+//ajustar cajas
+function ajustarCajas() {
+  var imagenes = $( '.imagen-caja' );
+  if( imagenes.length > 0 ) {
+    var altura = imagenes[0].height;
+    var cajas = $( 'div.contenido-caja' );
+    $( cajas ).each( function ( i, elemento ) {
+      $( elemento ).css( { 'height': altura + 'px' } ); 
+    }); 
+  }
+}
--- wp-content/themes/lapizzeria/page-nosotros.php
+++ wp-content/themes/lapizzeria/page-nosotros.php
@@ -16,14 +16,29 @@
       </main>
     </div>
 
-    <div class="informacion-cajas contenedor">
+    <div class="informacion-cajas contenedor clear">
       <div class="caja">
-        <img src="<?php the_field('imagen_1'); ?>"/>
-      </div>
-      <div class="contenido-caja">
-        <?php the_field('descripcion_1'); ?>
-      </div>
-    </div>
+        <?php $imagen = wp_get_attachment_image_src( get_field('imagen_1'), 'nosotros' ); ?>
+        <img src="<?php echo $imagen[0]; ?>" class="imagen-caja" />
+        <div class="contenido-caja">
+          <?php the_field('descripcion_1'); ?>
+        </div>
+      </div><!--Caja 1-->
+      <div class="caja">
+        <div class="contenido-caja">
+          <?php the_field('descripcion_2'); ?>
+        </div>
+        <?php $imagen = wp_get_attachment_image_src( get_field('imagen_2'), 'nosotros' ); ?>
+        <img src="<?php echo $imagen[0]; ?>" class="imagen-caja" />
+      </div><!--Caja 2-->
+      <div class="caja">
+        <?php $imagen = wp_get_attachment_image_src( get_field('imagen_3'), 'nosotros' );  ?>
+        <img src="<?php echo $imagen[0]; ?>" class="imagen-caja" />
+        <div class="contenido-caja">
+          <?php the_field('descripcion_3'); ?>
+        </div>
+      </div><!--Caja 3-->
+    </div><!--Contenedor Cajas-->
     
     <?php endwhile; ?>
 
