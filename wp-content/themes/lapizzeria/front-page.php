1eb69acb1a704f521a39e236d4322f30b1a7469a
Rafael Soriano <rafa.soriano.ramirez@gmail.com>
2018-01-06 12:58:31 -0600

    Ajustes página principal

--- wp-content/themes/lapizzeria/front-page.php
+++ wp-content/themes/lapizzeria/front-page.php
@@ -1,13 +1,11 @@
 <?php get_header();?>
 
     <?php while( have_posts() ): the_post(); ?>
-
         <div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
           <div class="contenido-hero">
               <div class="texto-hero">
                 <h1><?php echo esc_html( get_option( 'blogdescription' ) ); ?></h1>
                 <?php the_content(); ?>
-
                 <?php $url = get_page_by_title( 'Sobre Nosotros' ); ?>
                 <a class="button naranja" href="<?php echo get_permalink( $url->ID ) ?>">Leer más</a>
               </div>
@@ -50,5 +48,40 @@
           ?>
         </main>
     </div>
+
+    <section class="ingredientes">
+      <div class="contenedor">
+        <div class="contenedor-grid">
+        <?php while( have_posts() ): the_post(); ?>
+            <div class="columnas2-4">
+              <?php the_field('contenido'); ?>
+              <p class="precio"> <?php the_field( 'precio' ); ?> </p>
+              <a href="<?php the_permalink(); ?>" class="button">Leer más</a>
+            </div>
+            <div class="columnas2-4 imagen">
+              <img src="<?php the_field('imagen'); ?>"/>
+            </div>
+          <?php endwhile; ?>
+        </div>
+      </div>
+    </section>
+
+    <div class="contenedor">
+      <h2 class="texto-rojo texto-centrado">Galería de imágenes</h2>
+      <?php $url = get_page_by_title( 'Galería' ); ?>
+      <?php echo get_post_gallery( $url->ID ); ?>
+    </div>
+
+    <section class="ubicacion-reservacion">
+      <div class="contenedor-grid">
+        <div class="columnas2-4">
+          mapa aquí
+        </div>
+        <div class="columnas2-4">
+          <?php get_template_part('templates/formulario', 'reservacion'); ?>
+        </div>
+      </div>
+    </section>
+
 
 <?php get_footer(); ?>
